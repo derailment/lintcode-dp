fdc3c22e6bee6431ce48da0fc5c4e0ee67bcd117
Abby Wu <abby.wu@qct.io>
2019-05-20 15:30:53 +0800

    Dynamic Programming on Partitioning

---
+++ coins_in_a_line.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+ * @param n: An integer
+ * @return: A boolean which equals to true if the first player will win
+ */
+func firstWillWin(n int) bool {
+	// write your code here
+}
+
+func main() {
+	fmt.Println(firstWillWin(1))
+}
---
+++ copy_books.go
@@ -0,0 +1,43 @@
+package main
+
+/**
+ * @param pages: an array of integers
+ * @param k: An integer
+ * @return: an integer
+ */
+func copyBooks(pages []int, k int) int {
+	n := len(pages)
+	f := make([][]int, n+1)
+	costSum := make([]int, n)
+	costSum[0] = pages[0]
+	for j := 0; j < n+1; j++ {
+		if j != 0 && j != 1 {
+			costSum[j-1] = costSum[j-2] + pages[j-1]
+		}
+		f[j] = make([]int, k+1)
+		for p := 1; p < k+1; p++ {
+			if j == 0 {
+				f[j][p] = 0
+			} else if p == 1 {
+				f[j][p] = costSum[j-1]
+			} else {
+				minCost := int(^uint(0) >> 1)
+				for i := 1; i <= j; i++ {
+					partition := max(f[i][p-1], costSum[j-1]-costSum[i-1])
+					if partition < minCost {
+						minCost = partition
+					}
+				}
+				f[j][p] = minCost
+			}
+		}
+	}
+	return f[n][k]
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
