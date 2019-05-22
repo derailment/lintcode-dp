38ae8222272508a89825b4bed8280ac61fb3ac4d
Abby Wu <abby.wu@qct.io>
2019-05-22 12:04:31 +0800

    Dynamic Programming on Game Theory

--- coins-in-a-line3.go
+++ coins-in-a-line3.go
@@ -9,9 +9,21 @@ import (
  * @return: a boolean which equals to true if the first player will win
  */
 func firstWillWin3(values []int) bool {
-
+	n := len(values)
+	f := make([][]int, n)
+	for i := n - 1; i >= 0; i-- {
+		f[i] = make([]int, n)
+		for j := i; j < n; j++ {
+			if i == j {
+				f[i][j] = values[i]
+			} else {
+				f[i][j] = max(values[i]-f[i+1][j], values[j]-f[i][j-1])
+			}
+		}
+	}
+	return f[0][n-1] >= 0
 }
 
 func main() {
-	fmt.Println(firstWillWin3([]int{3, 2, 2}))
+	fmt.Println(firstWillWin3([]int{1, 20, 4}))
 }
